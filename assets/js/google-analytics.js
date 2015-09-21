6d204377ec0643cb894a26fe8ce2da129e765e47
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-09-21 20:59:22 +0200

    its working, woot

--- assets/js/google-analytics.js
+++ assets/js/google-analytics.js
@@ -3,5 +3,5 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', HYN_ANALYTICS_GOOGLE, 'auto');
+ga('create', '%%TRACKING_CODE%%', 'auto');
 ga('send', 'pageview');
--- src/Listeners/AddTrackingJs.php
+++ src/Listeners/AddTrackingJs.php
@@ -4,6 +4,7 @@ use Flarum\Events\RegisterLocales;
 use Flarum\Events\BuildClientView;
 use Illuminate\Contracts\Events\Dispatcher;
 use Flarum\Core\Settings\SettingsRepository;
+use Flarum\Forum\Actions\ClientAction as ForumClientAction;
 
 class AddTrackingJs
 {
@@ -26,10 +27,10 @@ class AddTrackingJs
     public function addAssets(BuildClientView $event)
     {
         if($event->action instanceof ForumClientAction) {
-            if($this->settings('hyn.analytics.google')) {
-                $rawJs = file_get_contents(__DIR__ . '/assets/js/analytics.js');
-                $js    = str_replace("%%TRACKING_CODE%%", $this->settings('hyn.analytics.google'), $rawJs);
-                $event->view->getAssets()->addJs($js);
+            if($this->settings->get('hyn.analytics.google')) {
+                $rawJs = file_get_contents(realpath(__DIR__ . '/../../assets/js/google-analytics.js'));
+                $js    = str_replace("%%TRACKING_CODE%%", $this->settings->get('hyn.analytics.google'), $rawJs);
+                $event->view->getAssets()->addJs(function() use ($js) { return $js; });
             }
         }
     }
