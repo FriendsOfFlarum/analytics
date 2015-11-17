99cca90bac107c4d827943238a4840cff3b899e4
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-11-17 21:03:25 +0100

    minor namespace change

--- js/admin/src/main.js
+++ js/admin/src/main.js
@@ -4,5 +4,5 @@ import app from 'flarum/app';
 import AnalyticsSettingsModal from 'hyn/analytics/components/AnalyticsSettingsModal';
 
 app.initializers.add('hyn-analytics', app => {
-  app.extensionSettings.analytics = () => app.modal.show(new AnalyticsSettingsModal());
+  app.extensionSettings['hyn-analytics'] = () => app.modal.show(new AnalyticsSettingsModal());
 });
