0b9d17757ce3ce4a17ac5a331807f2405e52c623
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-11-19 23:50:20 +0100

    updated code to inject tracking code for Google

--- src/Listeners/AddTrackingJs.php
+++ src/Listeners/AddTrackingJs.php
@@ -27,7 +27,7 @@ class AddTrackingJs
             if($this->settings->get('hyn.analytics.google')) {
                 $rawJs = file_get_contents(realpath(__DIR__ . '/../../assets/js/google-analytics.js'));
                 $js    = str_replace("%%TRACKING_CODE%%", $this->settings->get('hyn.analytics.google'), $rawJs);
-                $event->view->getAssets()->addJs(function() use ($js) { return $js; });
+                $event->view->addFootString($js);
             }
         }
     }
