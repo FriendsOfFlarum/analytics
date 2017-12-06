69d990f2ffdab699254b5505d472eee2e964657f
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-12-06 22:48:30 +0100

    Fix formatting

--- src/Listeners/AddClientAssets.php
+++ src/Listeners/AddClientAssets.php
@@ -17,17 +17,16 @@ class AddClientAssets
 
     public function addAssets(ConfigureWebApp $event)
     {
-        if($event->isAdmin()) {
+        if ($event->isAdmin()) {
             $event->addAssets([
                 __DIR__ . '/../../less/admin/analyticsPage.less',
                 __DIR__ . '/../../js/admin/dist/extension.js',
             ]);
 
             $event->addBootstrapper('flagrow/analytics/main');
-        } elseif($event->isForum())
-        {
+        } elseif ($event->isForum()) {
             $event->addAssets([
-                __DIR__ . '/../../js/forum/dist/extension.js'
+                __DIR__ . '/../../js/forum/dist/extension.js',
             ]);
 
             $event->addBootstrapper('flagrow/analytics/main');
@@ -36,9 +35,9 @@ class AddClientAssets
 
     public function addLocales(ConfigureLocales $event)
     {
-        foreach (new DirectoryIterator(__DIR__.'/../../locale') as $file) {
+        foreach (new DirectoryIterator(__DIR__ . '/../../locale') as $file) {
             if ($file->isFile() && in_array($file->getExtension(), ['yml', 'yaml'])) {
-                $event->locales->addTranslations($file->getBasename('.'.$file->getExtension()), $file->getPathname());
+                $event->locales->addTranslations($file->getBasename('.' . $file->getExtension()), $file->getPathname());
             }
         }
     }
