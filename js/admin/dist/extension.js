1989aaf77fd00153e90a94bd6544ac57202c583a
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-12-06 22:29:21 +0100

    Fix analytics page for urls that include protocol

--- js/admin/dist/extension.js
+++ js/admin/dist/extension.js
@@ -60,10 +60,22 @@ System.register('flagrow/analytics/components/AnalyticsPage', ['flarum/Component
                 babelHelpers.createClass(AnalyticsPage, [{
                     key: 'view',
                     value: function view() {
+                        var piwikUrl = app.data.settings['flagrow.analytics.piwikUrl'];
+
+                        // Use protocol-relative url if the url contains no protocol
+                        if (piwikUrl.indexOf('http://') === -1 && piwikUrl.indexOf('https://') === -1 && piwikUrl.indexOf('//') === -1) {
+                            piwikUrl = '//' + piwikUrl;
+                        }
+
+                        // Add trailing slash if necessary
+                        if (piwikUrl[piwikUrl.length - 1] !== '/') {
+                            piwikUrl += '/';
+                        }
+
                         //Call the piwik application
-                        this.url = '//' + m.prop(app.data.settings['flagrow.analytics.piwikUrl'])() + '/index.php';
-                        this.url += '?idSite=' + m.prop(app.data.settings['flagrow.analytics.piwikSiteId'])();
-                        this.url += '&token_auth=' + m.prop(app.data.settings['flagrow.analytics.piwikAuthToken'])();
+                        this.url = piwikUrl + 'index.php';
+                        this.url += '?idSite=' + app.data.settings['flagrow.analytics.piwikSiteId'];
+                        this.url += '&token_auth=' + app.data.settings['flagrow.analytics.piwikAuthToken'];
                         this.url += '&module=Widgetize&action=iframe&moduleToWidgetize=Dashboard&actionToWidgetize=index&period=month&date=today';
 
                         return [m('div', { className: 'analyticsPage' }, [m('div', { className: 'piwik' }, [m('label', ['Piwik']), m('iframe', {
--- js/admin/src/components/AnalyticsPage.js
+++ js/admin/src/components/AnalyticsPage.js
@@ -3,10 +3,22 @@ import Component from 'flarum/Component';
 export default class AnalyticsPage extends Component {
 
     view() {
+        let piwikUrl = app.data.settings['flagrow.analytics.piwikUrl'];
+
+        // Use protocol-relative url if the url contains no protocol
+        if (piwikUrl.indexOf('http://') === -1 && piwikUrl.indexOf('https://') === -1 && piwikUrl.indexOf('//') === -1) {
+            piwikUrl = '//' + piwikUrl;
+        }
+
+        // Add trailing slash if necessary
+        if (piwikUrl[piwikUrl.length - 1] !== '/') {
+            piwikUrl += '/';
+        }
+
         //Call the piwik application
-        this.url = '//' + m.prop(app.data.settings['flagrow.analytics.piwikUrl'])() + '/index.php';
-        this.url += '?idSite=' + m.prop(app.data.settings['flagrow.analytics.piwikSiteId'])();
-        this.url += '&token_auth=' + m.prop(app.data.settings['flagrow.analytics.piwikAuthToken'])();
+        this.url = piwikUrl + 'index.php';
+        this.url += '?idSite=' + app.data.settings['flagrow.analytics.piwikSiteId'];
+        this.url += '&token_auth=' + app.data.settings['flagrow.analytics.piwikAuthToken'];
         this.url += '&module=Widgetize&action=iframe&moduleToWidgetize=Dashboard&actionToWidgetize=index&period=month&date=today';
 
         return [
