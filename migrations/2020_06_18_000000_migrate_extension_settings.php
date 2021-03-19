07a0e286fc4cd786dee5a8cded084a03381b1a33
Ian Morland <imorland@users.noreply.github.com>
2021-03-19 08:28:42 +0000

    Apply fixes from StyleCI

--- migrations/2020_06_18_000000_migrate_extension_settings.php
+++ migrations/2020_06_18_000000_migrate_extension_settings.php
@@ -3,9 +3,9 @@
 /*
  * This file is part of fof/analytics.
  *
- * Copyright (c) 2021 FriendsOfFlarum.
+ * Copyright (c) 2020 - 2021 FriendsOfFlarum.
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
