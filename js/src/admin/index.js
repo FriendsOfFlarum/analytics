3b1223bf6ebb1c5b4618d238769028ad6f11e119
IanM <ian@morland.me>
2025-10-19 08:06:19 +0100

    chore: misc

--- js/src/admin/index.js
+++
@@ -1,6 +0,0 @@
-import app from 'flarum/admin/app';
-import AnalyticsSettingsPage from './components/AnalyticsSettingsPage';
-
-app.initializers.add('fof-analytics', (app) => {
-  app.registry.for('fof-analytics').registerPage(AnalyticsSettingsPage);
-});
